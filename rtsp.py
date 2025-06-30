0d501c6c277e1bbf8f00c57ff850698cee785766
spahicadev <adis@spahica.com>
2025-06-30 15:08:20 +0200

    Implement RabbitMQ message broker between RTSP Microservice and AI Microservice

--- rtsp.py
+++ rtsp.py
@@ -1,10 +1,14 @@
 import cv2
-import base64
 import threading
 import redis
+import pika
 import json
 
+#Connections
 r = redis.Redis(host='192.168.1.100', port=6379, decode_responses=True)
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.100'))
+channel = connection.channel()
+channel.queue_declare(queue="rtsp_microservice", durable=True)
 
 #Class for stream objects
 class Rtsp:
@@ -28,47 +32,45 @@ class Rtsp:
 
         ret, buffer = cv2.imencode('.jpg', frame)
 
+        jpg_bytes = buffer.tobytes()
+
         if not ret:
             print("Logika za reconnect")
 
-        self.buffer = base64.b64encode(buffer).decode('utf-8')
+        channel.basic_publish(exchange='', routing_key='rtsp_microservice', body=jpg_bytes, properties=pika.BasicProperties(
+            headers={
+                'id': self.id,
+                'roi': json.dumps(self.roi),
+                'capacity': self.capacity,
+            }
+        ))
 
         cap.release()
-        cv2.destroyAllWindows()
 
-    def get_data(self):
-        return {
-            "id": self.id,
-            "roi": self.roi,
-            "capacity": self.capacity,
-            "frame": self.buffer,
-        }
+#Creating Rtsp (stream) instances
 
-#Creating stream instances
+stream1 = Rtsp("Parking1", "rtsp://192.168.1.71/profile1", [(785.5973813420617, 1285.7610474631742), (912.1658483360604,
+          1323.2951445717395), (1062.302236770321, 1365.1936715766494), (1247.3540643753402, 1412.329514457173), (1462.0840152755036,
+          1237.7523186033816), (1297.9814511729396, 1216.8030551009267), (1156.5739225313685, 1195.8537915984716), (1033.4969994544456,
+          1173.1587561374788)], 3)
 
-stream1 = Rtsp("Parking1", "rtsp://192.168.1.71/profile1", [(727.9869067103104, 1262.1931260229123), (866.7757774140747,
-          1304.9645390070914), (1018.6579378068733, 1348.608837970539), (1174.0316421167477, 1388.7615930169113), (1400.9819967266767,
-          1219.4217130387335), (981.996726677577, 1151.3366066557548)], 3)
-
-stream2 = Rtsp("Parking2", "rtsp://192.168.1.111:1024/h264_ulaw.sdp",  [
-    (82.29254740910731, 1265.6601038772787), (367.5443894190116,
-          1258.7027418770372), (659.7535934291576, 1253.484720376856),
-    (942.1065346056279, 1244.788017876554), (833.6876434351968,
-          990.26452470105), (622.6476627612025, 992.5836453677972),
-    (409.8683415871479, 995.4825462012312), (199.98792124652718,
-          990.8443048677368)
-        ], 3)
+stream2 = Rtsp("Parking2", "rtsp://192.168.1.111:1024/h264_ulaw.sdp",  [(60.826032540675804, 1564.5572590738411), (365.3785982478095,
+          1555.2643929912379), (668.2415519399244, 1548.505944931163), (963.0788485607002, 1537.1010638297862), (840.5819774718392,
+          1265.0735294117637), (625.5788485607004, 1264.651126408009), (409.7309136420523, 1268.0303504380468), (192.61576971214004,
+          1274.7887984981217)], 3)
 
 rtsp_list = [stream1, stream2]
 
 #Reading frames
-
 def read_frames():
     for rtsp in rtsp_list:
         rtsp.get_frame()
-        r.set(rtsp.id, json.dumps(rtsp.get_data()))
 
     threading.Timer(5.0, read_frames).start()
+
+
+if __name__ == "__main__":
+    read_frames()
 
 
 
